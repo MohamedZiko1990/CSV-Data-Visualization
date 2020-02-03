49474fd32488e578c811092295b08814ac928538
Khalafallah <Mohamed.khalafallah8@gmail.com>
2020-02-03 11:37:53 +0200

    first Structure

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TextForm from "./TextForm";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <TextForm />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/TextForm.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class TextForm extends React.Component {
+  onFormSubmit() {}
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" />
+          <button type="submit"> Process </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default TextForm;
