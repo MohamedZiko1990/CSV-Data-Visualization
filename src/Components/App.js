2ea29035049322b1da6dd43644a6c37582413f81
Khalafallah <Mohamed.khalafallah8@gmail.com>
2020-02-03 12:24:11 +0200

    processing CSV Data

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { convertCSVToArray } from "convert-csv-to-array";
 import TextForm from "./TextForm";
 
 class App extends React.Component {
+  state = { objArray: null, headers: [] };
+
+  onDataSubmit = data => {
+    const arrayofObjects = convertCSVToArray(data, {
+      separator: ","
+    });
+    console.log(arrayofObjects);
+    console.log(arrayofObjects[0]);
+    this.setState({ objArray: arrayofObjects, headers: arrayofObjects[0] });
+  };
+
   render() {
     return (
       <div>
-        <TextForm />
+        <TextForm onSubmitCSV={this.onDataSubmit} />
       </div>
     );
   }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,14 +1,36 @@
 import React from "react";
 
 class TextForm extends React.Component {
-  onFormSubmit() {}
+  state = { data: null };
+  onFormSubmit = (event, data) => {
+    event.preventDefault();
+    console.log(data);
+    this.props.onSubmitCSV(data);
+  };
+  onChangeData = event => {
+    console.log(event.target.value);
+    this.setState({ data: event.target.value });
+  };
 
   render() {
     return (
       <div>
-        <form>
-          <input type="text" />
-          <button type="submit"> Process </button>
+        <form
+          className="pl-3"
+          onSubmit={event => this.onFormSubmit(event, this.state.data)}
+        >
+          <div className="form-group shadow-textarea">
+            <label>
+              Place your CSV Data:
+              <textarea
+                className="form-control"
+                rows="3"
+                placeholder="Place your data..."
+                onChange={this.onChangeData}
+              ></textarea>
+            </label>
+          </div>
+          <button type="submit">Process</button>
         </form>
       </div>
     );
